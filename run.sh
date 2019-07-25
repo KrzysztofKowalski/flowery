24fe84eb400eeb0b33df086315235dfde0e9ce70
Krzysztof Kowalski <krzysztof7kowalski@gmail.com>
2019-07-25 04:54:47 +0200

    x

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env dash
 
+printf "%s" "Cleaniing..."
+
 time rm -fr out; 
 
+printf "%s" "Cleaned!"
+
+printf "%s" "Generation"
+
 time ./preparecommands.sh `pwd` out/images; 
+
+printf "%s" "Commands ready"
+
+printf "%s" "Launching"
 
 time ./launch.sh 
