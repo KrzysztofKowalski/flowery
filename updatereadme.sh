4481f1a348689f47030b4fd25caf84b41746128d
Krzysztof Kowalski <krzysztof7kowalski@gmail.com>
2019-07-27 23:35:10 +0200

    readme

--- updatereadme.sh
+++ updatereadme.sh
@@ -4,5 +4,5 @@ S='!'
 for a in $(ls -1 samples );
 do
   s=`printf "%s\n" $a | cut -d'-' -f2-5 | cut -d'.' -f1 | tr '-' ' '`
-  printf "Values X: %s Y: %s Z: %s, P: %s \n%s[%s Y: %s Z: %s, P: %s](./samples/%s)\n" $s "$S" $s "$a" >> README.md
+  printf "\nValues X: %s Y: %s Z: %s, P: %s \n%s[%s Y: %s Z: %s, P: %s](./samples/%s)\n" $s "$S" $s "$a" >> README.md
 done
