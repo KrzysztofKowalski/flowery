d9ab514b6535c910620ba5e8626c1964af29cb54
Krzysztof Kowalski <krzysztof7kowalski@gmail.com>
2019-07-27 22:40:09 +0200

    wonderful code

--- cx.sh
+++ cx.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env dash
 code=$0:t
 if [ $# -lt 4 ]; then goto help; fi
-n1=$1; n2=$2; n3=$3; a1=1.0; a2=1.0; a3=1.0; s1=0.0; s2=0.0; s3=0.0; output=$4; samples=$5
+n1=$1; n2=$2; n3=$3; output=$4; samples=$5
+fname=$(printf "%s/spiro-%04d-%04d-%04d-%0d.svg" "${output}" "${n1}" "${n2}" "${n3}" "${samples}")
+if [ -f "${fname}" ]; then exit; fi
+a1=1.0; a2=1.0; a3=1.0; s1=0.0; s2=0.0; s3=0.0
 if [ $# -gt 6 ]; then a1=${6}; fi
 if [ $# -gt 7 ]; then a2=${7}; fi
 if [ $# -gt 8 ]; then a3=${8}; fi
 if [ $# -gt 9 ]; then s1=${9}; fi
 if [ $# -gt 10 ]; then s2=${10}; fi
 if [ $# -gt 11 ]; then s3=${11}; fi
-aformat=`printf "%04d" $n1`; bformat=`printf "%04d" $n2`; cformat=`printf "%04d" $n3`
-fname="${output}/spiro-${aformat}-${bformat}-${cformat}-${samples}.svg"
-if [ -f $fname ]
-then
-  # printf "%s generated already!\n" $fname
-  exit
-fi
 cat <<EOF | gnuplot
  set size ratio -1
  set nokey
